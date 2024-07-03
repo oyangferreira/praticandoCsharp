cb80479c2d2d0a27ea23f254be479915b21478a9
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-03 06:11:54 -0300

    Atualização

--- Media/Program.cs
+++ Media/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double valor1, valor2, media;
+
+Console.Write("Digite o 1°valor: ");
+valor1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o 2°valor: ");
+valor2 = double.Parse(Console.ReadLine());
+
+media = (valor1 + valor2) / 2;
+
+Console.WriteLine("Média = "+media);
+
