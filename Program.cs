9c8ae918a69dbc60e622a99375d9664999c6fee7
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-03 05:47:37 -0300

    Adicionado fórmula de número par ou ímpar

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int valor;
+
+Console.Write("Digite um número: ");
+valor = int.Parse(Console.ReadLine());
+
+if(valor % 2 == 0) {
+    Console.WriteLine("Par");
+}
+else {
+    Console.WriteLine("Ímpar");
+}
